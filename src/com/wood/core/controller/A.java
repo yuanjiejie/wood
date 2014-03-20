2a0928b3edf9e175d4bc5f4dd45e5663b05511b4
pc <pc@pc-PC>
2014-03-20 18:33:14 +0800

    清理

--- src/com/wood/core/controller/A.java
+++ src/com/wood/core/controller/A.java
@@ -1,11 +1,5 @@
 package com.wood.core.controller;
 
 public class A {
-	public static void main(String[] args) {
-		System.out.println("可以提交了");
-		System.out.println("看看能不能同步");
-		System.out.println("OK");
-		System.out.println("OK");
-	}
 
 }
